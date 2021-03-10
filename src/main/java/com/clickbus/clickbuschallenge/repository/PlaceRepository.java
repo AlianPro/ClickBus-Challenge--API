6d06cac096a4b7a4c6fa760ca7aef99bc969cc99
AlianPro <alianpro>
2021-03-10 16:11:31 -0300

    Challenge Finished

--- src/main/java/com/clickbus/clickbuschallenge/repository/PlaceRepository.java
+++ src/main/java/com/clickbus/clickbuschallenge/repository/PlaceRepository.java
@@ -2,10 +2,12 @@ package com.clickbus.clickbuschallenge.repository;
 
 import com.clickbus.clickbuschallenge.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PlaceRepository extends JpaRepository<Place,Long> {
 
+    @Query("SELECT place FROM Place place WHERE place.name LIKE %:name%")
     List<Place> findByName(String name);
 }
